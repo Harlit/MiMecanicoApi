3a4877ac78a499dd8f70898e632eb3c5fc95958f
Harlit <harlit.estevez@gmail.com>
2023-03-23 18:03:49 -0400

    Agregar archivos de proyecto.

---
+++ MiMecanico/Models/Mecanicos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiMecanico.Models
+{
+    public class Mecanicos
+    {
+        [Key]
+        public int MecanicoId { get; set; }
+        public string? Nombre { get; set; }
+        public string? Area { get; set; }
+        public Boolean Disponible { get; set; }
+        
+    }
+}
