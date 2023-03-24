fd0af7ab5734280566d9fbcca73e924b58f67fce
Harlit <harlit.estevez@gmail.com>
2023-03-24 06:28:23 -0400

    se hizo la migracion y la base de datos

--- MiMecanico/Data/Contexto.cs
+++ MiMecanico/Data/Contexto.cs
@@ -1,6 +1,22 @@
-﻿namespace MiMecanico.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MiMecanico.Models;
+
+namespace MiMecanico.Data
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Mecanicos> Mecanicos => Set<Mecanicos>();
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
     }
 }
